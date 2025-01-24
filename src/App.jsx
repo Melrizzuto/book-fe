c0cf55a60ecd28fbc4dbfef406fa5dada3f8a6d4
Melrizzuto <melrizzuto4@gmail.com>
2025-01-24 10:21:38 +0100

    set up front end

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1>Homepage</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+export default function Homepage() {
+    return (
+        <>
+            <h1>Sono Homepage</h1>
+            <p>qui vedremo tutti i libri</p>
+        </>
+
+    )
+}
