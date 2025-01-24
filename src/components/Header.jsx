77691225f66fc932290050892ef73459544ee915
Orsouene <orsoueneelaouizeb@gmail.com>
2025-01-24 11:45:55 +0100

    Fatto il navBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,33 @@
+import { NavLink } from "react-router-dom";
 export default function Header() {
-    return (
-        <h1>Sono Header</h1>
-    )
+  return (
+    <header>
+      <ul className="nav d-flex justify-content-center gap-5  ">
+        <li>
+          <NavLink to="/" className="myList major-mono-display-regular">
+            Homepage
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/Book" className="myList major-mono-display-regular">
+            Book
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/ContactUs"
+            className="myList major-mono-display-regular"
+          >
+            Contact us
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/About" className="myList major-mono-display-regular">
+            About
+          </NavLink>
+        </li>
+      </ul>
+    </header>
+  );
 }
