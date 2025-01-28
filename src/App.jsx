b5cedd3a055ee484b8967a4dba480620e76964ee
eleonoracappellesso <cappellessoeleonora@gmail.com>
2025-01-28 14:32:22 +0100

    fixed errors with global provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GlobalProvider } from "./contexts/GlobalContext";
 
 import DefaultLayout from "./layout/DefaultLayout";
 import HomePage from "./pages/Homepage";
@@ -6,21 +7,25 @@ import BookDetails from "./pages/BookDetails";
 import Form from "./components/Form";
 import ContactUs from "./pages/ContactUs";
 import About from "./pages/About";
+
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route Component={DefaultLayout}>
-          <Route path="/books">
-            <Route index Component={HomePage} />
-            <Route path=":id" Component={BookDetails} />
+    <GlobalProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route path="/books">
+              <Route index Component={HomePage} />
+              <Route path=":id" Component={BookDetails} />
+            </Route>
+            <Route path="/form" Component={Form} />
+            <Route path="/contact" Component={ContactUs} />
+            <Route path="/about" Component={About} />
           </Route>
-          <Route path="/form" Component={Form} />
-          <Route path="/contact" Component={ContactUs} />
-          <Route path="/about" Component={About} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </GlobalProvider>
   );
 }
 
