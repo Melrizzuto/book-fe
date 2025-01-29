4180385be44afa7e95c54623c1d976acbfa58df2
Melrizzuto <melrizzuto4@gmail.com>
2025-01-29 20:13:58 +0100

    updated resposivity added media queries

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,23 +1,18 @@
+import styles from './Jumbotron.module.css';
+
 export default function Jumbotron({ onDiscoverMore }) {
     return (
-        <div
-            className="hero hero-wave has-overlap-img has-overlap-img-with-text"
-            style={{
-                backgroundColor: "rgb(181, 198, 172)",
-                height: "100vh",
-                minHeight: "100vh"
-            }}
-        >
-            <div className="container h-100">
-                <div className="row align-items-center h-100">
-                    <div className="col-lg-5 offset-xxl-1 col-xxl-6 mb-4 mb-lg-0 text-center text-lg-left">
-                        <p className="h2 font-weight-normal mb-0">
+        <div className={styles.hero}>
+            <div className={`${styles.heroContainer} container`}>
+                <div className="col-lg-5 offset-xxl-1 col-xxl-6 mb-4 mb-lg-0">
+                    <div className={styles.heroText}>
+                        <p className={`${styles.heroTitle} font-weight-normal mb-0`}>
                             The nerdiest
                             <br />
                             italian <i>book community</i>.
                         </p>
                         <br />
-                        <p className="h2 mb-4">
+                        <p className={`${styles.heroSubtitle} mb-4`}>
                             We help you read reviews of
                             <br />
                             your favorite books.
@@ -25,24 +20,18 @@ export default function Jumbotron({ onDiscoverMore }) {
                         {/* Pulsante "Scopri di più" */}
                         <button
                             onClick={onDiscoverMore}
-                            className="btn btn-primary text-white font-weight-bold text-uppercase"
-                            style={{
-                                backgroundColor: "#aba9ee",
-                                borderRadius: "20px",
-                                padding: "10px 20px",
-                                border: "white"
-                            }}
+                            className={`${styles.heroButton} btn text-white`}
                         >
                             Discover more
                         </button>
                     </div>
-                    <div className="col-lg-7 col-xxl-5 z-index-9 position-relative">
-                        <img
-                            src="https://assets.acciobooks.com/images/home-hero-illustration.svg"
-                            className="hero-illustration img-fluid w-100"
-                            alt="Hero Illustration"
-                        />
-                    </div>
+                </div>
+                <div className={`${styles.heroImageWrapper} col-lg-7 col-xxl-5`}>
+                    <img
+                        src="https://assets.acciobooks.com/images/home-hero-illustration.svg"
+                        className={`${styles.heroImage} img-fluid`}
+                        alt="Hero Illustration"
+                    />
                 </div>
             </div>
         </div>
